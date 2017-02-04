bde6d8d9f785e9330ccdd906678fcf117ac1635e
Jongnam Woo <woojn90@gmail.com>
2017-02-04 17:04:33 +0900

    issue #1 complete commit
    complete #1 'Basic Structure'

---
+++ app/src/main/java/com/android/woojn/coursebookmarkapplication/SettingsActivity.java
@@ -0,0 +1,33 @@
+package com.android.woojn.coursebookmarkapplication;
+
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+/**
+ * Created by wjn on 2017-02-04.
+ */
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        ActionBar actionBar = this.getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            NavUtils.navigateUpFromSameTask(this);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}

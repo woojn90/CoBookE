031445b36996d4579cea2576c7d3978bd3b890b7
woojn90 <woojn90@gmail.com>
2017-02-04 17:18:57 +0900

    Basic Structure (#2)
    * Update README.md
    
    * issue #1 complete commit
    
    complete #1 'Basic Structure'
    
    * accept tutor's advice
    
    remove whitespace

---
+++ app/src/main/java/com/android/woojn/coursebookmarkapplication/MainActivity.java
@@ -0,0 +1,34 @@
+package com.android.woojn.coursebookmarkapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_setting) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
